028138ad1d3131457ddb5bb63ad9457eecdfef34
Kumar Ayush <cheekujodhpur@gmail.com>
2016-07-06 10:44:40 +0530

    hello world

---
+++ client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Client program
+
+import socket
+
+HOST = 'wncc-iitb.org'   # The remote host
+PORT = 7991              # The same port as server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send(b'Hello, world')
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Server program
+
+import socket
+
+HOST = ''   #all interfaces
+PORT = 7991
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    conn.send(data)
+conn.close()
