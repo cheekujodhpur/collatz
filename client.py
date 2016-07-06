650c63b87137a76aa05bdbfd120039270370a413
Kumar Ayush <cheekujodhpur@gmail.com>
2016-07-06 12:27:30 +0530

    decorated thigns

--- client.py
+++ client.py
@@ -7,14 +7,15 @@ import struct
 import sys
 
 HOST = '127.0.0.1'   # The remote host
-PORT = 7991              # The same port as server
+PORT = 7991          # The same port as server
+ADDR = (HOST, PORT)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 count = 0
 for i in range(1000000):
-    s.sendto(struct.pack('<L', sys.getsizeof(str(i))), (HOST, PORT))
-    s.sendto(str(i), (HOST, PORT))
+    s.sendto(struct.pack('<L', sys.getsizeof(str(i))), ADDR)
+    s.sendto(str(i), ADDR)
     count = count+1
 
 print "Sent", count
-s.sendto(struct.pack('<L', 0), (HOST, PORT))
+s.sendto(struct.pack('<L', 0), ADDR)
--- server.py
+++ server.py
@@ -7,13 +7,19 @@ import struct
 
 HOST = ''   #all interfaces
 PORT = 7991
+ADDR = (HOST, PORT)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
+s.bind(ADDR)
 
 count = 0
 while True:
-    num_len = struct.unpack('<L', s.recvfrom(struct.calcsize('<L'))[0])[0]
-    if num_len == 0: break
-    data, addr = s.recvfrom(num_len)
+    # read the header, size of incoming number
+    try:
+        num_len = struct.unpack('<L', s.recvfrom(struct.calcsize('<L'))[0])[0]
+    except ValueError:
+        print "Corrupt header"
+
+    data, remote_addr = s.recvfrom(num_len)
+    print data
     if data.isdigit(): count = count + 1
 print "Received", count
